8fbfb2bd9b96ec7d13c3aef7d08befdd4d35f8d6
Zakhar Shapurau <zakhar@mcash.no>
2014-06-11 23:23:10 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcash.recipe.appengine',
+    version='0.1',
+    author="mCASH Team",
+    author_email="support@mca.sh",
+    description="Buildout recipe for google appengine",
+    entry_points={'zc.buildout': ['default = appengine:Recipe']}
+)
+
